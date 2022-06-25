7d6cae060afb5425542cbc94d2990039a156df86
Vihang Patil <vihang.patil@gmail.com>
2022-06-25 12:11:22 +0200

    Dependency updates

--- ksp/build.gradle.kts
+++ ksp/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn")
+        freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn")
     }
 }
 
